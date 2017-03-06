80c1198c6677c41c06f3503f5cfff616659f1299
ChangedNameTo <william.m95@gmail.com>
2017-03-06 01:04:34 -0500

    user #comment Model working, added bootstrap, CRU working

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+
+  # Validation
+  validates :first_name, presence: true,
+                         length: { minimum: 1 }
+
+  validates :last_name, presence: true,
+                        length: { minimum: 1 }
+
+  validates :email_address, presence: true,
+                            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+																			message: "Not a valid email address" }
+
+  validates :phone_number, presence: true,
+                           length: { minimum: 1 }
+
+  # Model Functions
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+end
