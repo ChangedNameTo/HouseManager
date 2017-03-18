62664293b2fb37be182d3350638eb241be147efb
ChangedNameTo <william.m95@gmail.com>
2017-03-17 20:41:10 -0400

    organizations #comment Member lists are working

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,17 @@
 class Organization < ApplicationRecord
   has_one :user, foreign_key: 'organization_manager'
+
+  def organization_members
+    users = []
+    users.concat(get_organization_members(self))
+  end
+
+  def get_organization_members(user)
+    members = []
+    User.where(organization_id: self.id).where.not(organization_id: nil).each do |member|
+      members.append(member)
+    end
+
+    members
+  end
 end
