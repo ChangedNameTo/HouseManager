d375bb45dd71012f838f93f0e1c837629cce30c5
ChangedNameTo <william.m95@gmail.com>
2017-03-27 23:37:45 -0400

    lateplates #comment Fixed the time select

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,6 +2,10 @@ class Meal < ApplicationRecord
   has_many :late_plates
 
   def meal_name
-    "#{name} at #{time}"
+    "#{name} at #{short_time}"
+  end
+
+  def short_time
+    time.to_s(:time)
   end
 end
