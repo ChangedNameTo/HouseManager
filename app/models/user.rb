489ec7d877b835673a381fd3d165317673f64de3
ChangedNameTo <william.m95@gmail.com>
2017-04-05 09:05:06 -0400

    testing #comment I now have fancy models, guard live reloads, and there is cool testing stuff ready to work on

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                      :integer          not null, primary key
+#  first_name              :string           not null
+#  last_name               :string           not null
+#  phone_number            :string
+#  enabled                 :boolean          default(FALSE), not null
+#  email_address           :string           not null
+#  provider                :string           not null
+#  uid                     :string           not null
+#  oauth_token             :string           not null
+#  oauth_expires_at        :datetime         not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  affiliated_organization :integer
+#
+
 class User < ApplicationRecord
 
   # Scopes
